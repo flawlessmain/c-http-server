f85277bc2a1d72731269cc0c087ade6230ca003f
flawlessmain <sobka.lol@mail.ru>
2025-08-01 16:57:48 +0300

    created read-print-loop

--- lisp.c
+++ lisp.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#define TRUE 1
 
-int main(void *args){
+static char input[2048];
 
-    printf("Hello\n");
+int main(int argc, char** argv){
+    puts("My own Lisp version 0.0.1");
+    puts("Press ctrl+c to exit.");
+
+    while (TRUE) {
+        fputs("lisp> ", stdout);
+        fgets(input, 2048, stdin);
+        printf("No, you are a %s", input);
+    }
     return 0;
 }

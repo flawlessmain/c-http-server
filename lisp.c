60617ed30848e59e50cd35044fef0d74247cd75b
flawlessmain <sobka.lol@mail.ru>
2025-08-01 18:04:55 +0300

    added win support

--- lisp.c
+++ lisp.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
-
 #define TRUE 1
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char *readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else 
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv){
     puts("My own Lisp version 0.0.1");

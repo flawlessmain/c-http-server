ec2a614c88b678ac3b3a41f78d7c1d20f2b5997a
flawlessmain <sobka.lol@mail.ru>
2025-08-02 19:48:05 +0300

    added a parse library

--- lisp.c
+++ lisp.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "mpc.h"
+
 #define TRUE 1
 
 #ifdef _WIN32
@@ -23,15 +25,68 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+long eval_op(long x, char* op, long y){
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t){
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+    char* op = t->children[1]->contents;
+    long x = eval(t->children[2]);
+    int i = 3;
+    while(strstr(t->children[i]->tag, "expr")){
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
 int main(int argc, char** argv){
     puts("My own Lisp version 0.0.1");
     puts("Press ctrl+c to exit.");
 
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lisp = mpc_new("lisp");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                               \
+         number     :   /-?[0-9]+/  ;                                   \
+         operator   :   '+' | '-' | '*' | '/'   ;                       \
+         expr       :   <number>    |   '(' <operator>  <expr>+')'    ; \
+         lisp      :   /^/ <operator>   <expr>+ /$/ ;                   \
+         ",
+        Number, Operator, Expr, Lisp);
+    
+
+    mpc_result_t r;
+    long result; 
+
     while (TRUE) {
+        
         char *input = readline("lisp> ");
         add_history(input);
-        printf("%s\n", input);
+        
+        if (mpc_parse("<stdin>", input, Lisp, &r)) {
+            result = eval(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         free(input);
     }
+    
+    mpc_cleanup(4, Number, Operator, Expr, Lisp);
+
     return 0;
 }
